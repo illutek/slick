a69398ab6c838cb84f3f7d5c182477135b4ce231
Vandenborne Stefan <stefan@illutek.be>
2015-03-08 22:45:46 +0100

    na wat theming tot dit reslutaat, is nog wat werk aan 20150308

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Stefan on 8/03/2015.
+ */
+
+$(document).ready(function(){
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+Een test op een slick slider http://kenwheeler.github.io/slick/  als bedoeling om dit als foto album
+te gebruiken in een Drupal site
